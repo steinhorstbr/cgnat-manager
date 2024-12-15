e1bdd6948f42817fbeb334ec0c496e5d64600978
RODRIGO TIAGO STEINHORST TRICHES <49219129+steinhorstbr@users.noreply.github.com>
2024-12-15 19:44:00 -0300

    Update routes.js

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Main from './pages/Main/Main';
 import Login from './pages/Login/Login';
@@ -7,15 +7,16 @@ import Home from './pages/Home/Home';
 import Logs from './pages/Logs/Log';
 import Setting from './pages/Setting/Setting';
 
-export default function Routes() {
+export default function AppRoutes() {
     return (
         <BrowserRouter>
-            <Route path="/" exact component={Main} />
-            <Route path="/login" exact component={Login} />
-            <Route path="/home" exact component={Home} />
-            <Route path="/logs" exact component={Logs} />
-            <Route path="/setting" exact component={Setting} />
+            <Routes>
+                <Route path="/" element={<Main />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/logs" element={<Logs />} />
+                <Route path="/setting" element={<Setting />} />
+            </Routes>
         </BrowserRouter>
     );
 }
-
